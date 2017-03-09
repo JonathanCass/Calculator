26197bb79779293ea9d6f5b96df7c389768f5b23
JonCass <JCass1980@gmail.com>
2017-03-09 15:05:08 -0800

    making progress

--- AdventureMode/main.js
+++ AdventureMode/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
 
+	var inputString =""
+	var input1
+	var input2
+	var output1 =""
+
+	$(".digits").click(function(){
+		output1 += this.name
+		$("#output").val(output1)
+		})
+	$(".operations").click(function(){
+		output1 += this.name
+		$("#output").val(output1)
+		})	
 })

ca249f84240ccb2f46a241981f3e74a26c647479
JonCass <JCass1980@gmail.com>
2017-03-09 15:52:30 -0800

    working on equals button operation

--- AdventureMode/main.js
+++ AdventureMode/main.js
@@ -3,14 +3,36 @@ $(document).ready(function(){
 	var inputString =""
 	var input1
 	var input2
-	var output1 =""
+	var output1 = ""
+	var outputArray = []
+	var oaCounter = 0
+	var checker = true //when checker is true calculator is fresh / cleared 
+	// therefore the array is empty and can be pushed to, after that set to false and numbers are stacked in
+	// when stack is cleared or total is calculated then set checker to false
 
 	$(".digits").click(function(){
-		output1 += this.name
+		output1 += $(this).attr('name')
+		if(checker){
+			outputArray.push($(this).attr('name'))
+			checker=false
+		}
+		else{
+			outputArray[oaCounter] += $(this).attr('name')
+		}
 		$("#output").val(output1)
-		})
+		console.log(oaCounter)
+		console.log(outputArray)
+	})
 	$(".operations").click(function(){
-		output1 += this.name
+		output1 += $(this).attr('name')
+		outputArray.push($(this).attr('name'))
 		$("#output").val(output1)
-		})	
+		oaCounter += 2
+		checker = true
+		console.log(oaCounter)
+		console.log(outputArray)
+	})
+	$("#equals").click(function(){
+		$("#output").val("")	
+	})			
 })
